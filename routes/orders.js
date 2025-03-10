e806dd4d821c788919377c90e7a22000e78bf6b1
kim song yun <rlatjddbs531@naver.com>
2025-03-10 13:14:33 +0900

    첫 번째 커밋

---
+++ routes/orders.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(express.json());
+
+//주문하기
+router.post('/', (req,res) => {
+    res.json('주문 성공');
+});
+
+//주문 목록 조회
+router.get('/', (req,res) => {
+    res.json('주문 목록 조회');
+});
+
+//주문 상세 상품 조회
+router.get('/:id', (req,res) => {
+    res.json('주문 상세 상품 조회');
+});
+
+
+module.exports = router
